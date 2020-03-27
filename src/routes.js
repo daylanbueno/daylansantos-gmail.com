541004e158c115797bb7fb176d6ae393648b6ad7
Dailan Bueno <daylansantos@gmail.com>
2020-03-26 21:51:16 -0300

    Recuperar incidents por ong locado passando ong_id no headers

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const ongcontroller = require("./controllers/OngController");
 const incidenteController = require("./controllers/IncidenteController");
+const profileController = require("./controllers/ProfileController");
 
 routes.post("/ongs", ongcontroller.create);
 routes.get("/ongs", ongcontroller.index);
@@ -10,5 +11,7 @@ routes.get("/ongs", ongcontroller.index);
 routes.post("/incidentes", incidenteController.create);
 routes.get("/incidentes", incidenteController.index);
 routes.delete("/incidentes/:id", incidenteController.delete);
+
+routes.get("/profile", profileController.index);
 
 module.exports = routes;
